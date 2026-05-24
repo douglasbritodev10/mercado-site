248c541d12780fb674e264246b9a1b323245d1b8
Douglas Brito <douglasbrito.dev.10@gmail.com>
2026-05-23 21:09:35 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,28 @@
-const CACHE_NAME = 'casacanil-v2';
+const CACHE_NAME = 'v2_casacanil';
+
+// Apenas arquivos que REALMENTE existem no seu GitHub
 const assets = [
+  './',
+  './index.html',
   './pagina.html',
+  './clientes.html',
+  './historico.html',
   './manifest.json',
   './icon-192.jpeg',
   './icon-512.jpeg'
 ];
 
-self.addEventListener('install', (e) => {
+self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Instalando cache...');
       return cache.addAll(assets);
     })
   );
-  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
